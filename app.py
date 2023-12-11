910b0b8e291ba342228c461948bc68c44410b8d7
yousuf shahzad <yousufshahzadd@yahoo.com>
2023-12-11 21:00:47 +0000

    fixed issue, mapped all strs to ints instead as str values was not working properly

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import io, base64
 from flask import Flask, request, render_template
 from bell_curve import create_curve
@@ -18,22 +19,27 @@ def curve_generator():
     if request.method == 'POST':
         mxscore = request.form['mxscore']
         mnscore = request.form['mnscore']
-        input_list = request.form['input_list']
+        input_list_str = request.form['input_list']
         select_variables = True
-        if mxscore == '' or mnscore == '' or input_list == '':
+        if mxscore == '' or mnscore == '' or input_list_str == '':
             error = 'Please fill in the forms below'
         else:
             print(mxscore)
             print(mnscore)
-            print(input_list)
+            print(input_list_str)
+            input_list = list(map(int, input_list_str.split(','))) # map all strs in the list to ints
+            if select_variables == True:
+                fig = create_curve(input_list, mxscore, mnscore, 7)
+                # Convert plot to PNG image
+                pngImage = io.BytesIO()
+                FigureCanvas(fig).print_png(pngImage)
 
-            fig = create_curve(input_list, mxscore, mnscore, 7)
-            # Convert plot to PNG image
-            pngImage = io.BytesIO()
-            FigureCanvas(fig).print_png(pngImage)
-            
-            # Encode PNG image to base64 string
-            pngImageB64String = "data:image/png;base64,"
-            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+                # Encode PNG image to base64 string
+                pngImageB64String = "data:image/png;base64,"
+                pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+            else:
+                pass
 
     return render_template('curve-generator.html', error=error, image=pngImageB64String)
+
+# Rest of your code...
--- bell_curve.py
+++ bell_curve.py
@@ -11,47 +11,46 @@ def generate_grade_boundaries(marks: list, num_grades: int):
     return percentiles
     
 def create_curve(data, mxscore: int, mnscore: int, num_grades: int) -> Figure:
-    if app.select_variables == True:
-        max_input_marks = int(mxscore)
-        input_marks = np.array(data)
-        marks = input_marks * (100/max_input_marks)
-        max_marks = max_input_marks * (100/max_input_marks)
-        num_grades = 7
-        grade_boundaries = generate_grade_boundaries(marks, num_grades)
-        mean = np.mean(marks)
-        std_dev = np.std(marks)
-        x = np.linspace(min(marks), 100, 1000)
-        y = norm.pdf(x, mean, std_dev)
-        
-        fig = Figure(figsize=(8, 6))
-        FigureCanvasAgg(fig)  # Create a canvas for the figure
-        ax = fig.add_subplot(111)   
-        
-        ax.plot(x, y, color='black', linestyle='dashed', label='Bell Curve')
-        colors = ['darkred', 'red', 'orange', 'yellow', 'yellowgreen', 'lime', 'cyan']
-        for i in range(2, len(grade_boundaries)):
-            boundary = grade_boundaries[i]
-            ax.axvline(x=boundary, linestyle='--', color='black')
-            if i == 2:
-                ax.fill_between(x, 0, y, where=(x <= boundary), color=colors[i - 2], alpha=0.3)
-            else:
-                prev_boundary = grade_boundaries[i - 1]
-                ax.fill_between(x, 0, y, where=((x > prev_boundary) & (x <= boundary)), color=colors[i - 2], alpha=0.3)
-        for i in range(2, len(grade_boundaries)):
-            boundary = grade_boundaries[i]
-            if i == 2:
-                area_percentage = round(np.trapz(y[np.where(x <= boundary)[0]]) / np.trapz(y), 3) * 100
-                ax.text((boundary + min(marks)) / 2, 0.00025, f'Grade: {i + 1} | {area_percentage:.1f}%', ha='center', va='bottom', color='black', rotation='vertical')
-            else:
-                prev_boundary = grade_boundaries[i - 1]
-                area_percentage = round(np.trapz(y[np.where((x > prev_boundary) & (x <= boundary))[0]]) / np.trapz(y), 3) * 100
-                ax.text(prev_boundary + (boundary - prev_boundary) / 2, 0.00025, f'Grade: {i + 1} | {area_percentage:.1f}%', ha='center', va='bottom', color='black', rotation='vertical')
-        ax.set_xticks(np.arange(0, 100 + 1, 5))
-        ax.set_xlabel('Marks')
-        ax.set_ylabel('Probability Density')
-        ax.set_title('Bell Curve for Student Marks')
-        ax.grid(True)
-        ax.margins(0,0)
-        return fig
+    max_input_marks = int(mxscore)
+    input_marks = np.array(data)
+    marks = input_marks * (100/max_input_marks)
+    max_marks = max_input_marks * (100/max_input_marks)
+    num_grades = 7
+    grade_boundaries = generate_grade_boundaries(marks, num_grades)
+    mean = np.mean(marks)
+    std_dev = np.std(marks)
+    x = np.linspace(min(marks), 100, 1000)
+    y = norm.pdf(x, mean, std_dev)
+    
+    fig = Figure(figsize=(8, 6))
+    FigureCanvasAgg(fig)  # Create a canvas for the figure
+    ax = fig.add_subplot(111)   
+    
+    ax.plot(x, y, color='black', linestyle='dashed', label='Bell Curve')
+    colors = ['darkred', 'red', 'orange', 'yellow', 'yellowgreen', 'lime', 'cyan']
+    for i in range(2, len(grade_boundaries)):
+        boundary = grade_boundaries[i]
+        ax.axvline(x=boundary, linestyle='--', color='black')
+        if i == 2:
+            ax.fill_between(x, 0, y, where=(x <= boundary), color=colors[i - 2], alpha=0.3)
+        else:
+            prev_boundary = grade_boundaries[i - 1]
+            ax.fill_between(x, 0, y, where=((x > prev_boundary) & (x <= boundary)), color=colors[i - 2], alpha=0.3)
+    for i in range(2, len(grade_boundaries)):
+        boundary = grade_boundaries[i]
+        if i == 2:
+            area_percentage = round(np.trapz(y[np.where(x <= boundary)[0]]) / np.trapz(y), 3) * 100
+            ax.text((boundary + min(marks)) / 2, 0.00025, f'Grade: {i + 1} | {area_percentage:.1f}%', ha='center', va='bottom', color='black', rotation='vertical')
+        else:
+            prev_boundary = grade_boundaries[i - 1]
+            area_percentage = round(np.trapz(y[np.where((x > prev_boundary) & (x <= boundary))[0]]) / np.trapz(y), 3) * 100
+            ax.text(prev_boundary + (boundary - prev_boundary) / 2, 0.00025, f'Grade: {i + 1} | {area_percentage:.1f}%', ha='center', va='bottom', color='black', rotation='vertical')
+    ax.set_xticks(np.arange(0, 100 + 1, 5))
+    ax.set_xlabel('Marks')
+    ax.set_ylabel('Probability Density')
+    ax.set_title('Bell Curve for Student Marks')
+    ax.grid(True)
+    ax.margins(0,0)
+    return fig
     
 
